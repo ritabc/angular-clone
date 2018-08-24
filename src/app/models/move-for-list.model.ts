3e045c4078df00ecc4062814bdc3cd205f874094
ritabc <ritabc@gmail.com>
2018-08-23 20:09:58 -0700

    Update MoveForList Model class structure and redo move data

---
+++ src/app/models/move-for-list.model.ts
@@ -0,0 +1,5 @@
+export class MoveForList {
+  constructor(
+  public name: string,
+  public category: string, public timesAround: string[]) { }
+}
--- src/app/models/move.model.ts
+++
@@ -1,3 +0,0 @@
-export class Move {
-  constructor(public description: string, public category: string, public count: number) { }
-}
