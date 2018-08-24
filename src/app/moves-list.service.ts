d1d332144d0e75fa6347fab7482d6f1b30e2f9bc
ritabc <ritabc@gmail.com>
2018-08-24 14:49:33 -0700

    show all moves from Firebase db in the moves-to-choose component

--- src/app/moves-list.service.ts
+++ src/app/moves-list.service.ts
@@ -1,8 +1,16 @@
 import { Injectable } from '@angular/core';
+import { MoveForList } from './models/move-for-list.model';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
 
 @Injectable()
 export class MovesListService {
+  moves: FirebaseListObservable<any[]>;
+  constructor(private database: AngularFireDatabase) {
+    this.moves = database.list('listOfMoves');
+  }
 
-  constructor() { }
+  getMoves() {
+    return this.moves;
+  }
 
 }
--- src/app/moves-to-choose/moves-to-choose.component.ts
+++ src/app/moves-to-choose/moves-to-choose.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-
+import { MoveForList } from '../models/move-for-list.model';
+import { Router } from '@angular/router';
+import { MovesListService } from '../moves-list.service';
+import { FirebaseListObservable } from 'angularfire2/database';
 @Component({
   selector: 'app-moves-to-choose',
   templateUrl: './moves-to-choose.component.html',
-  styleUrls: ['./moves-to-choose.component.css']
+  styleUrls: ['./moves-to-choose.component.css'],
+  providers: [MovesListService]
 })
 export class MovesToChooseComponent implements OnInit {
+  movesForList: FirebaseListObservable<any[]>;
 
-  constructor() { }
+  constructor(private router: Router, private movesListService: MovesListService) { }
 
   ngOnInit() {
+    this.movesForList = this.movesListService.getMoves();
   }
 
 }
